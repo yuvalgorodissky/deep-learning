bc23a32875a5bc6da75dfb823e179a8f62484482
noamazulay <noamaz@post.bgu.ac.il>
2024-07-05 13:14:01 +0300

    fix evaluation

--- Assignment 3/src/evaluation.py
+++ Assignment 3/src/evaluation.py
@@ -23,20 +23,13 @@ from evaluation_utils import *
 def get_args():
     parser = argparse.ArgumentParser(description="Train LSTM Seq2Seq model for lyrics generation")
     parser.add_argument('--midi_path', type=str, required=True, help="Path to the MIDI files directory")
-    parser.add_argument('--lyrics_path', type=str, required=True, help="Path to the lyrics CSV file")
     parser.add_argument('--test_path', type=str, required=True, help="Path to the test set CSV file")
     parser.add_argument('--models_save_path', type=str, required=True, help="Path to save the trained model")
-    parser.add_argument('--writer_path', type=str, required=True, help="Path for TensorBoard logs")
     parser.add_argument('--input_size_encoder', type=int, default=128, help="Input size for the encoder")
     parser.add_argument('--hidden_size_encoder', type=int, default=256, help="Hidden size for the encoder")
     parser.add_argument('--input_size_decoder', type=int, default=300, help="Input size for the decoder")
     parser.add_argument('--hidden_size_decoder', type=int, default=512, help="Hidden size for the decoder")
     parser.add_argument('--num_layers', type=int, default=2, help="Number of layers in the LSTM")
-    parser.add_argument('--learning_rate', type=float, default=0.001, help="Learning rate for the optimizer")
-    parser.add_argument('--batch_size', type=int, default=4, help="Batch size for training")
-    parser.add_argument('--epochs', type=int, default=30, help="Number of training epochs")
-    parser.add_argument('--melody_strategy', type=str, default="piano_roll", help="Melody strategy")
-    parser.add_argument('--teacher_forcing', type=float, default=0.5, help="Teacher forcing ratio")
     parser.add_argument('--output_dir', type=str, default="output", help="Output directory")
 
     return parser.parse_args()
@@ -65,7 +58,7 @@ def main():
 
         model.to(device)
         test_dataloader = get_dataloader(lyrics_path=args.test_path, midis_path=args.midi_path,
-                                         batch_size=args.batch_size,
+                                         batch_size=1,
                                          word2vec_model=model.word2vec, word_to_index=model.word_to_index,
                                          vocabulary=model.vocabulary,
                                          melody_strategy=model_melody_strategy, shuffle=False)
